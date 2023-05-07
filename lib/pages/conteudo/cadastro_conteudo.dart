e0e55b2d68842bee69a1660d23bd259e279a476a
Erick Cardoso <erickc8@hotmail.com>
2023-05-07 03:28:56 -0300

    form conteudo fields

--- lib/pages/conteudo/cadastro_conteudo.dart
+++ lib/pages/conteudo/cadastro_conteudo.dart
@@ -20,44 +20,90 @@ class _ContentPageState extends State<ContentPage> {
   final _descricaoController = TextEditingController();
 
   int _selectedIndex = 0;
-  
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children : [
-            CustomTextField(controller: _tituloController, hintText: 'Título'),
-            const SizedBox(
-              height: 15,
-              width: 15,
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.all(20.0),
+            child: const Text(
+              'Cadastrar Conteúdo',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
             ),
-            CustomTextField(controller: _subtituloController, hintText: 'Subtítulo'),
-            const SizedBox(
-              height: 15,
-              width: 15,
+          ),
+          const SizedBox(
+            height: 15,
+            width: 15,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  CustomTextField(controller: _tituloController, hintText: 'Título'),
+                  const SizedBox(
+                    height: 15,
+                    width: 15,
+                  ),
+                  CustomTextField(
+                    controller: _subtituloController,
+                    hintText: 'Subtítulo',
+                  ),
+                  const SizedBox(
+                    height: 15,
+                    width: 15,
+                  ),
+                  CustomDropdownButton(
+                    controller: _lesaoController,
+                    hintText: 'Selecionar Lesão',
+                    list: const [
+                      'Tendinite',
+                      'Bursite',
+                      'Miosites',
+                      'Tenossinovite',
+                      'Sinovite',
+                      'Lumbago'
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                    width: 15,
+                  ),
+                  TextField(
+                    controller: _descricaoController,
+                    decoration: InputDecoration(
+                      hintText: 'Descrição',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      )
+                    ),
+                    maxLines: 4),
+                  const SizedBox(
+                    height: 15,
+                    width: 15,
+                  ),
+                  ElevatedButton(
+                    child: const Text('Salvar'),
+                    onPressed: () {},
+                    ),
+                ],
+              ),
             ),
-            CustomDropdownButton(controller: _lesaoController, hintText: 'Selecionar Lesão', list: const [
-              'Tendinite',
-              'Bursite',
-              'Miosites',
-              'Tenossinovite',
-              'Sinovite',
-              'Lumbago'
-            ],
-            ),
-            const SizedBox(
-              height: 15,
-              width: 15,
-            ),
-          ],  
-      ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
