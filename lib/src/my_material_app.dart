5dd4db50dc4c69e1e7b676dfcaf4cf8ddcc01a9d
dvdgd <dvd.gd762@gmail.com>
2023-06-26 19:03:40 -0300

    feat: add routine page

--- lib/src/my_material_app.dart
+++ lib/src/my_material_app.dart
@@ -6,6 +6,7 @@ import 'package:prev_ler/src/modules/home/home_page.dart';
 import 'package:prev_ler/src/modules/injuries/pages/register_injury.dart';
 import 'package:prev_ler/src/modules/main/main_page.dart';
 import 'package:prev_ler/src/modules/profile/profile_page.dart';
+import 'package:prev_ler/src/modules/routines/pages/routine_form_page.dart';
 import 'package:prev_ler/src/modules/routines/pages/routine_page.dart';
 import 'package:prev_ler/src/shared/controllers/dark_mode_controller.dart';
 import 'package:prev_ler/src/shared/utils/enums.dart';
@@ -85,6 +86,10 @@ class MyMaterialApp extends StatelessWidget {
             ),
         '/profile': (_) => const ProfilePage(),
         '/routines': (_) => const RoutinesPage(),
+        '/routines/register': (_) => RoutineFormPage(
+              title: 'Nova Rotina',
+              routine: null,
+            )
       },
     );
   }
