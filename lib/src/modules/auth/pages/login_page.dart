29b41d8fe186584202b763bcb28f207e9de96125
dvdgd <dvd.gd762@gmail.com>
2023-06-27 22:58:55 -0300

    feat: form validation in login page

--- lib/src/modules/auth/pages/login_page.dart
+++ lib/src/modules/auth/pages/login_page.dart
@@ -17,6 +17,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   late final AuthController controller;
 
@@ -44,6 +45,19 @@ class _AuthPageState extends State<AuthPage> {
     }
   }
 
+  Future<void> _makeLogin() async {
+    final formState = _formKey.currentState;
+    if (formState == null || !formState.validate()) {
+      return;
+    }
+
+    final loginModel = AuthRequestModel(
+      _emailController.text,
+      _passwordController.text,
+    );
+    await controller.login(loginModel);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,55 +68,58 @@ class _AuthPageState extends State<AuthPage> {
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                'Bem vindo!',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              const SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  'Faça o login ou cadastre-se para continuar...',
-                  softWrap: true,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyMedium,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Text(
+                  'Bem vindo!',
+                  style: Theme.of(context).textTheme.headlineLarge,
                 ),
-              ),
-              const SizedBox(height: 30),
-              MyTextFormField(
-                controller: _emailController,
-                labelText: 'Email',
-                prefixIcon: const Icon(Icons.email_outlined),
-                textInputType: TextInputType.emailAddress,
-              ),
-              PasswordField(
-                controller: _passwordController,
-                labelText: 'Senha',
-              ),
-              const SizedBox(height: 30),
-              MyFilledLoadingButton(
-                text: 'Login',
-                action: () => controller.login(
-                  AuthRequestModel(
-                    _emailController.text,
-                    _passwordController.text,
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    'Faça o login ou cadastre-se para continuar...',
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ),
-              ),
-              const SizedBox(height: 15.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _buildTextButton('Registrar médico', '/register/medic'),
-                    _buildTextButton('Registrar paciente', '/register/patient'),
-                  ],
+                const SizedBox(height: 30),
+                MyTextFormField(
+                  controller: _emailController,
+                  validator: (text) =>
+                      text == null || text.isEmpty ? "Preencha o email." : null,
+                  labelText: 'Email',
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  textInputType: TextInputType.emailAddress,
                 ),
-              ),
-            ],
+                PasswordField(
+                  validator: (text) =>
+                      text == null || text.isEmpty ? "Digite a senha." : null,
+                  controller: _passwordController,
+                  labelText: 'Senha',
+                ),
+                const SizedBox(height: 30),
+                MyFilledLoadingButton(
+                  text: 'Login',
+                  action: _makeLogin,
+                ),
+                const SizedBox(height: 15.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildTextButton('Registrar médico', '/register/medic'),
+                      _buildTextButton(
+                          'Registrar paciente', '/register/patient'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
